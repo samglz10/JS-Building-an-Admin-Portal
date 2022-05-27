dab79a8d4275741ce38bfd48b7db9afde7ea370d
Samuel Gonzalez <samglz10@gmail.com>
2022-05-27 19:27:36 -0400

    first update

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,11 @@
 
 // Your Code Here
+const main= async() => {
+    const response = await fetch('http://localhost:3001/listBooks')
+    let books = await response.json();
+    books.forEach(renderBook);
+}
+
+const renderBook=(book) =>{
+
+} 
